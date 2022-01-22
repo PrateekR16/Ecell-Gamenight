4a6f6e33a57530009394951ed9342ca606159c3f
lokita282 <lokitavarma282@gmail.com>
2022-01-23 02:52:49 +0530

    UI improved

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
+import React from 'react';
 import Table from './components/Table'
 import AppBar from './components/AppBar'
+import Heading from './components/Heading'
+import ParticlesContainer from './components/ParticlesContainer'
+
 function App() {
+  
   return (
-    <div className="App">
-      <AppBar></AppBar>
-      <Table></Table>
-    </div>
-  );
+    <>
+      <div
+        style={{
+          backgroundColor: '	#202020',
+          height: '100vh',
+        }}
+      >
+        <Heading/>
+        <AppBar></AppBar>
+        <Table></Table>
+      </div>
+      <div
+        style={{
+          opacity: '0.25',
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <ParticlesContainer />
+      </div>
+    </>
+  )
 }
+
+
+
 
 export default App;
