b8be6e22b0d4b5630dea0d83ef7fb0e988844c5e
PrateekR16 <prateekranka1607@gmail.com>
2022-01-23 00:31:58 +0530

    data entry success

--- src/components/Table.js
+++ src/components/Table.js
@@ -9,52 +9,48 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import { useState, useEffect } from 'react'
 import axios from 'axios';
-function createData(rank, teamName, points) {
-  return { rank, teamName, points };
-}
-
-const rows = [
-  createData(1, 'Team Name1', 500),
-  createData(2, 'Team Name2', 400),
-  createData(3, 'Team Name3', 200),
-  createData(4, 'Team Name4', 300),
-  createData(5, 'Team Name5', 100),
-];
 
 export default function BasicTable() {
-  useEffect(() => {
 
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    
     const getData = async () => {
       let res = await axios.get('https://sheets.googleapis.com/v4/spreadsheets/1digOYBypnO4-5ZtCibBfbULwEkSllpNNG8ZMduBqkME/values/Sheet1!A1:c10?key=AIzaSyB2oGgTTFbend-RafhynufvPhKflY7O8OI')
 
-      let data = res.data.values[0][1];
-      console.log(data);
+      let values = res.data.values;
+      console.log(values);
+      setData(values)
+      setLoading(false)
     }
-
+    
     getData()
-  })
+  },[])
+
+  if(isLoading)
+  {
+    return(
+      <div>Loading....</div>
+    )
+  }
+
   return (
     <Box sx={{ width: "50vw", alignItems: "center", justifyContent: "center" }}>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
-            <TableRow>
-              <TableCell>Rank</TableCell>
-              <TableCell align="center">Team Name</TableCell>
-              <TableCell align="right">Points</TableCell>
-            </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
+            {data.map((row) => (
               <TableRow
                 key={row.rank}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
-                <TableCell component="th" scope="row">
-                  {row.rank}
-                </TableCell>
-                <TableCell align="center">{row.teamName}</TableCell>
-                <TableCell align="right">{row.points}</TableCell>
+                {row.map((item) =>{
+                  return <TableCell align="center">{item}</TableCell>
+                } )}
               </TableRow>
             ))}
           </TableBody>
